7a5b36b420796d0d50b8bff1d234c2ed49c6f023
Ryan Jones <ryacjone@gmail.com>
2019-06-01 18:11:59 -0400

    added testing api client and added logic to set the base url

--- oanda_dotnet/client/Restv20Client.cs
+++ oanda_dotnet/client/Restv20Client.cs
@@ -1,13 +1,24 @@
-﻿using RestSharp;
+﻿using System;
+using RestSharp;
 using RestSharp.Authenticators;
 
 namespace oanda_dotnet.client
 {
     public class Restv20Client : RestClient
     {
-        public Restv20Client(string bearerToken)
+        public string Url => $"https://{SubDomain}.oanda.com/";
+        protected virtual string SubDomain => "ap-fxtrade";
+
+        public Restv20Client(string bearerToken) : base()
         {
+            this.BaseUrl = new Uri(Url);
             this.Authenticator = new JwtAuthenticator(bearerToken);
         }
+    }
+
+    public sealed class Restv20TestingClient : Restv20Client
+    {
+        protected override string SubDomain => "ap-fxpractice";
+        public Restv20TestingClient(string bearerToken) : base(bearerToken) { }
     }
 }
