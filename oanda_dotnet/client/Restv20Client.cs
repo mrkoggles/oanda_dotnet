bc536a5bd17c70980fa7d62f6b66edfd05a09a13
Ryan Jones <ryacjone@gmail.com>
2019-06-01 08:49:09 -0400

    - installed RestSharpSigned nuget; - built RestV20Client object that inherits from RestSharpClient

---
+++ oanda_dotnet/client/Restv20Client.cs
@@ -0,0 +1,13 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+
+namespace oanda_dotnet.client
+{
+    public class Restv20Client : RestClient
+    {
+        public Restv20Client(string bearerToken)
+        {
+            this.Authenticator = new JwtAuthenticator(bearerToken);
+        }
+    }
+}
