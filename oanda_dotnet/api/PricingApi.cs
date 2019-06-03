01dfb49471aef0a44aecebf4b864b89d6819b55e
Ryan Jones <ryacjone@gmail.com>
2019-06-02 21:12:44 -0400

    Moved some logic to a better class

--- oanda_dotnet/api/PricingApi.cs
+++ oanda_dotnet/api/PricingApi.cs
@@ -1,19 +1,9 @@
 ﻿using oanda_dotnet.client;
-using oanda_dotnet.model.pricing;
-using oanda_dotnet.model;
-using System.Collections.Generic;
-using System;
 
 namespace oanda_dotnet.api
 {
     public sealed class PricingApi : Restv20Api
     {
         public PricingApi(Restv20Client client) : base(client) { }
-
-        private static ICollection<T> GenerateCollection<T>(T t)
-            where T: new()
-        {
-            return new List<T>() { t };
-        }
     }
 }
