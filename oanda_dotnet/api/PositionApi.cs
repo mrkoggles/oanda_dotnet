2d36ce472617553ca3ca5eeacb8db76a184bf27a
Ryan Jones <ryacjone@gmail.com>
2019-06-08 11:44:33 -0400

    added api object

---
+++ oanda_dotnet/api/PositionApi.cs
@@ -0,0 +1,9 @@
+﻿using oanda_dotnet.client;
+
+namespace oanda_dotnet.api
+{
+    public class PositionApi : Restv20Api
+    {
+        public PositionApi(Restv20Client client) : base(client) { }
+    }
+}
