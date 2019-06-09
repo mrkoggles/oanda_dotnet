86ebd09d48dbce8a3757b8f5419248350957a0da
Ryan Jones <ryacjone@gmail.com>
2019-06-09 12:55:36 -0400

    Added Trade & Transaction Apis

---
+++ oanda_dotnet/api/TradeApi.cs
@@ -0,0 +1,9 @@
+﻿using oanda_dotnet.client;
+
+namespace oanda_dotnet.api
+{
+    public class TradeApi : Restv20Api
+    {
+        public TradeApi(Restv20Client client) : base(client) { }
+    }
+}
---
+++ oanda_dotnet/api/TransactionApi.cs
@@ -0,0 +1,9 @@
+﻿using oanda_dotnet.client;
+
+namespace oanda_dotnet.api
+{
+    public class TransactionApi : Restv20Api
+    {
+        public TransactionApi(Restv20Client client) : base(client) { }
+    }
+}
