9337e0aa21a70c57a9876c6595301519aef51d7b
Ryan Jones <ryacjone@gmail.com>
2019-06-09 10:43:26 -0400

    added AccountApi

---
+++ oanda_dotnet/api/AccountApi.cs
@@ -0,0 +1,9 @@
+﻿using oanda_dotnet.client;
+
+namespace oanda_dotnet.api
+{
+    public class AccountApi : Restv20Api
+    {
+        public AccountApi(Restv20Client client) : base(client) { }
+    }
+}
