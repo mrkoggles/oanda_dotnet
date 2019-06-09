22b7ccd3712ef30514c2432b0adac5743f0c5791
Ryan Jones <ryacjone@gmail.com>
2019-06-09 13:40:59 -0400

    Renamed endpoint and response files

--- oanda_dotnet/model/trade/Endpoints.cs
+++ oanda_dotnet/model/trade/Endpoints.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using oanda_dotnet.model.account;
+﻿using oanda_dotnet.model.account;
 using oanda_dotnet.model.transaction;
 using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace oanda_dotnet.model.trade
 {
---
+++ oanda_dotnet/model/trade/Responses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oanda_dotnet.model.trade
+{
+    public class GetTradesResponse : Restv20EndpointResponse
+    {
+
+    }
+}
