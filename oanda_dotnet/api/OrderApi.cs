002dbd289fe439dd05ea6b8027822b7070e32e87
Ryan Jones <ryacjone@gmail.com>
2019-06-09 12:53:38 -0400

    added OrderApi object

---
+++ oanda_dotnet/api/OrderApi.cs
@@ -0,0 +1,9 @@
+﻿using oanda_dotnet.client;
+
+namespace oanda_dotnet.api
+{
+    public class OrderApi : Restv20Api
+    {
+        public OrderApi(Restv20Client client) : base(client) { }
+    }
+}
