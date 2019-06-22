ba3f6eb5a3f5975a550810987545cd78721998ff
Ryan Jones <ryacjone@gmail.com>
2019-06-22 11:28:32 -0400

    added constructor for Transactionid

--- oanda_dotnet/model/transaction/primatives/TransactionId.cs
+++ oanda_dotnet/model/transaction/primatives/TransactionId.cs
@@ -11,17 +11,19 @@ namespace oanda_dotnet.model.transaction
     {
         private ulong _transactionId;
 
-        public static implicit operator ulong(TransactionId transactionId) => transactionId._transactionId;
-        public static implicit operator string(TransactionId transactionId) => transactionId.ToString();
-
-        public static implicit operator TransactionId(ulong transactionId) => new TransactionId { _transactionId = transactionId };
-
         [Obsolete("Needs error handling")]
-        public static implicit operator TransactionId(string transactionId)
+        public TransactionId(string transactionId)
         {
-            if (ulong.TryParse(transactionId, out ulong result)) { return new TransactionId { _transactionId = result }; }
+            if (ulong.TryParse(transactionId, out ulong result)) {  _transactionId = result; }
             else { throw new Exception(); }
         }
+
+
+
+        public static implicit operator ulong(TransactionId transactionId) => transactionId._transactionId;
+        public static implicit operator string(TransactionId transactionId) => transactionId.ToString();
+        public static implicit operator TransactionId(ulong transactionId) => new TransactionId { _transactionId = transactionId };
+        public static implicit operator TransactionId(string transactionId) => new TransactionId(transactionId);
 
         public override string ToString()
             => this._transactionId.ToString();
