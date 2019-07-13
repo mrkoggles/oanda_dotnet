279be91c68138e3273732ef0ce28a5a3140000da
Ryan Jones <ryacjone@gmail.com>
2019-07-13 17:07:59 -0400

    changed properties to get when requested

--- oanda_dotnet/api/StreamApi.cs
+++ oanda_dotnet/api/StreamApi.cs
@@ -10,37 +10,50 @@ namespace oanda_dotnet.api
     public abstract class StreamApi<T> : IDisposable
         where T : StreamEndpointRequest<T>, new()
     {
-        private Stream _stream;
         private Task<Stream> _retrieveStreamTask;
+        private Stream _stream;
         private StreamReader _streamReader;
-        private StreamClient _client;
-        public StreamEndpointRequest<T> Request { get; private set; }
+
+        private Stream Stream
+        {
+            get
+            {
+                if(_stream == null) { _stream = _retrieveStreamTask.Result; }
+                return _stream;
+            }
+        }
+
+        private StreamReader StreamReader
+        {
+            get
+            {
+                if (_streamReader == null) { _streamReader = new StreamReader(Stream, Encoding.UTF8, true, 1024, true); }
+                return _streamReader;
+            }
+        }
 
         public string Response
         {
             get
             {
-                if (_streamReader == null)
-                {
-                    _stream = _retrieveStreamTask.Result;
-                    _streamReader = new StreamReader(_stream, Encoding.UTF8, true, 1024, true);
-                }
-
                 string _response = null;
-                while (_streamReader.Peek() != -1)  //.Peek() returns -1 when there is currently nothing to read
+                while (StreamReader.Peek() != -1)  //.Peek() returns -1 when there is currently nothing to read
                 {
-                    var currentValue = _streamReader.ReadLine();
+                    var currentValue = StreamReader.ReadLine();
                     if (true) { _response = currentValue; } //replace with logic to make sure not a heartbeat object                    
                 }
                 return _response;
             }
         }
 
-        public StreamApi(StreamClient client, StreamEndpointRequest<T> request) { }
+        public StreamApi(StreamClient client, StreamEndpointRequest<T> request)
+        {
+            _retrieveStreamTask = client.Execute(request);
+        }
 
         public void Dispose()
         {
-            _stream?.Dispose();
+            _stream.Dispose();
             _streamReader?.Dispose();
             GC.SuppressFinalize(this);
         }
