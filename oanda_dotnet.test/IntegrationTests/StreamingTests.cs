560d51e2b28f03b6806c81898817c0cd9c62c868
Ryan Jones <ryacjone@gmail.com>
2019-07-13 10:49:29 -0400

    added stream client

---
+++ oanda_dotnet.test/IntegrationTests/StreamingTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.IO;
+
+namespace oanda_dotnet.test.IntegrationTests
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class StreamingTests : BaseTestClass
+    {
+        [TestMethod]
+        public void StreamClientExecution()
+        {
+            using (oanda_dotnet.client.StreamTestingClient client = new oanda_dotnet.client.StreamTestingClient(this.ApiKey))
+            {
+                try
+                {
+                    using (System.IO.Stream stream = client.GetStreamAsync($"/v3/accounts/{this.AccountId}/transactions/stream").Result)
+                    using (System.IO.StreamReader reader = new StreamReader(stream, Encoding.UTF8, true, 1024, true))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            var x = reader.ReadLine();
+                        }
+                    }
+
+                }
+                catch (Exception e)
+                {
+
+                    throw;
+                }
+            }                
+        }
+    }
+}
---
+++ oanda_dotnet/client/StreamClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace oanda_dotnet.client
+{
+    public class StreamClient : HttpClient
+    {
+        public string Url => $"https://{SubDomain}.oanda.com/";
+        protected virtual string SubDomain => "stream-api-fxtrade";
+
+        public StreamClient(string bearerToken) : base()
+        {
+            this.DefaultRequestHeaders.Add("Authorization", $"Bearer {bearerToken}");
+            this.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+            this.BaseAddress = new Uri(Url);
+        }
+    }
+
+    public sealed class StreamTestingClient : StreamClient
+    {
+        protected override string SubDomain => "stream-api-fxpractice";
+
+        public StreamTestingClient(string bearerToken) : base(bearerToken) { }
+    }
+}
